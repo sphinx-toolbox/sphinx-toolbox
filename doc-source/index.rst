d5effa1e906fd01be865d0ca6f3dc2075d037d16
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-09-27 19:51:19 +0100

    Bump version: 1.3.0 → 1.3.1

--- doc-source/index.rst
+++ doc-source/index.rst
@@ -93,7 +93,7 @@ sphinx-toolbox
 	:alt: GitHub top language
 
 .. |commits-since| github-shield::
-	:commits-since: v1.3.0
+	:commits-since: v1.3.1
 	:alt: GitHub commits since tagged version
 
 .. |commits-latest| github-shield::
--- sphinx_toolbox/__init__.py
+++ sphinx_toolbox/__init__.py
@@ -52,7 +52,7 @@ __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020 Dominic Davis-Foster"
 
 __license__: str = "MIT License"
-__version__: str = "1.3.0"
+__version__: str = "1.3.1"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = ["setup"]
