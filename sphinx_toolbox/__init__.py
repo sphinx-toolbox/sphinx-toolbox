0cf40ed2bbf9d3a839b71119501dfc70b2d78a9f
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-10-28 22:49:16 +0000

    Bump version v1.6.2 -> v1.7.0

--- sphinx_toolbox/__init__.py
+++ sphinx_toolbox/__init__.py
@@ -52,7 +52,7 @@ __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020 Dominic Davis-Foster"
 
 __license__: str = "MIT License"
-__version__: str = "1.6.2"
+__version__: str = "1.7.0"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = ["setup"]
