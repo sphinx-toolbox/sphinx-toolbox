b2ccbc7d3ea42613364ebe98223ca10e9fdb74d7
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2023-01-12 10:37:41 +0000

    Cap pygments version.
    Latest version resyults in output with no spaces.

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -3,5 +3,6 @@
 __all__ = ["extras_require"]
 
 extras_require = {
-		"testing": ["coincidence>=0.4.3", "pygments>=2.7.4"], "all": ["coincidence>=0.4.3", "pygments>=2.7.4"]
+		"testing": ["coincidence>=0.4.3", "pygments<=2.13.0,>=2.7.4"],
+		"all": ["coincidence>=0.4.3", "pygments<=2.13.0,>=2.7.4"]
 		}
