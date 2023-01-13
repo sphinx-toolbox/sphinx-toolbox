c5ba7e18c1c91bc08f61828f9f35f6845f26179b
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2023-01-13 10:53:45 +0000

    Fix issues module.
    GitHub changes things their end.

--- sphinx_toolbox/github/issues.py
+++ sphinx_toolbox/github/issues.py
@@ -325,7 +325,12 @@ def get_issue_title(issue_url: str) -> Optional[str]:
 
 	if r.status_code == 200:
 		soup = BeautifulSoup(r.content, "html5lib")
-		content = soup.find_all("span", attrs={"class": "js-issue-title"})[0].text
+		try:
+			content = soup.find_all("span", attrs={"class": "js-issue-title"})[0].text
+		except IndexError:
+			# As of 13 Jan 2023 GitHub seems to use a bidirectional text tag instead
+			content = soup.find_all("bdi", attrs={"class": "js-issue-title"})[0].text
+
 		content = xml.sax.saxutils.escape(content).replace('"', "&quot;")
 		return content.strip()
 
