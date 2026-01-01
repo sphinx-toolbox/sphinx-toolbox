78567ef0e2d8ea024546ce6f5dca20e87feeb9ff
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-01-01 19:42:13 +0000

    Convert test_get_variable_type to use pytest.mark.parametrize

--- tests/test_more_autodoc/test_variables.py
+++ tests/test_more_autodoc/test_variables.py
@@ -2,6 +2,7 @@
 from typing import Any, Dict, List, NamedTuple, Sequence, Type, Union, no_type_check  # noqa: F401
 
 # 3rd party
+import pytest
 from coincidence import PEP_563
 from domdf_python_tools.secrets import Secret
 from sphinx.ext.autodoc.directive import AutodocDirective
@@ -44,47 +45,81 @@ class Documenter(NamedTuple):
 	analyzer: Analyzer
 
 
-@no_type_check
-def test_get_variable_type():
-	assert get_variable_type(Documenter(Foo, ["Foo", 'a'], Analyzer({}))) == ":py:class:`str`"
-	assert get_variable_type(Documenter(Foo, ["Foo", 'b'], Analyzer({}))) == ":py:class:`int`"
-	assert get_variable_type(Documenter(Foo, ["Foo", 'c'], Analyzer({}))) == ":py:class:`float`"
-	assert get_variable_type(Documenter(Foo, ["Foo", 'd'], Analyzer({}))) == ":py:class:`float`"
-	assert get_variable_type(Documenter(Foo, ["Foo", 'e'], Analyzer({}))) == ":py:class:`~typing.List`"
+variable_params = [
+		((Documenter(Foo, ["Foo", 'a'], Analyzer({}))), ":py:class:`str`"),
+		((Documenter(Foo, ["Foo", 'b'], Analyzer({}))), ":py:class:`int`"),
+		((Documenter(Foo, ["Foo", 'c'], Analyzer({}))), ":py:class:`float`"),
+		((Documenter(Foo, ["Foo", 'd'], Analyzer({}))), ":py:class:`float`"),
+		((Documenter(Foo, ["Foo", 'e'], Analyzer({}))), ":py:class:`~typing.List`"),
 
-	if PEP_563:
 		# On 3.10 with PEP 563 failed forward references break things earlier
-		assert get_variable_type(Documenter(Bar, ["Bar", 'a'], Analyzer({}))) == ":py:obj:`~.str`"
-		assert get_variable_type(Documenter(Bar, ["Bar", 'b'], Analyzer({}))) == ":py:obj:`~.int`"
-		assert get_variable_type(Documenter(Bar, ["Bar", 'c'], Analyzer({}))) == ":py:obj:`~.float`"
-	else:
-		assert get_variable_type(Documenter(Bar, ["Bar", 'a'], Analyzer({}))) == ":py:class:`str`"
-		assert get_variable_type(Documenter(Bar, ["Bar", 'b'], Analyzer({}))) == ":py:class:`int`"
-		assert get_variable_type(Documenter(Bar, ["Bar", 'c'], Analyzer({}))) == ":py:class:`float`"
+		pytest.param(
+				(Documenter(Bar, ["Bar", 'a'], Analyzer({}))),
+				":py:obj:`~.str`",
+				marks=pytest.mark.skipif(not PEP_563, reason="Output differs post PEP 563"),
+				),
+		pytest.param(
+				(Documenter(Bar, ["Bar", 'b'], Analyzer({}))),
+				":py:obj:`~.int`",
+				marks=pytest.mark.skipif(not PEP_563, reason="Output differs post PEP 563"),
+				),
+		pytest.param(
+				(Documenter(Bar, ["Bar", 'c'], Analyzer({}))),
+				":py:obj:`~.float`",
+				marks=pytest.mark.skipif(not PEP_563, reason="Output differs post PEP 563"),
+				),
+		pytest.param(
+				(Documenter(Bar, ["Bar", 'a'], Analyzer({}))),
+				":py:class:`str`",
+				marks=pytest.mark.skipif(PEP_563, reason="Output differs pre PEP 563"),
+				),
+		pytest.param(
+				(Documenter(Bar, ["Bar", 'b'], Analyzer({}))),
+				":py:class:`int`",
+				marks=pytest.mark.skipif(PEP_563, reason="Output differs pre PEP 563"),
+				),
+		pytest.param(
+				(Documenter(Bar, ["Bar", 'c'], Analyzer({}))),
+				":py:class:`float`",
+				marks=pytest.mark.skipif(PEP_563, reason="Output differs pre PEP 563"),
+				),
 
-	# Failed forward reference throws everything else out of whack
-	assert get_variable_type(Documenter(Bar, ["Bar", 'd'], Analyzer({}))) == ":py:obj:`~.float`"
-	assert get_variable_type(Documenter(Bar, ["Bar", 'e'], Analyzer({}))) == ":py:obj:`~.List`"
-	assert get_variable_type(Documenter(Bar, ["Bar", 'f'], Analyzer({}))) == ":py:obj:`~.Set`"
-	assert get_variable_type(Documenter(Bar, ["Bar", 'g'], Analyzer({}))) == ''
-	# assert get_variable_type(Documenter(Bar, ["Bar", "g"], Analyzer({("Bar", "g"): "Secret"}))) == ":py:class:`~domdf_python_tools.secrets.Secret`"
-	assert get_variable_type(
-			Documenter(Bar, ["Bar", 'h'], Analyzer({("Bar", 'h'): "'Secret'"}))
-			) == ":py:class:`~domdf_python_tools.secrets.Secret`"
-	assert get_variable_type(
-			Documenter(Bar, ["Bar", 'h'], Analyzer({("Bar", 'h'): '"Secret"'}))
-			) == ":py:class:`~domdf_python_tools.secrets.Secret`"
-	assert get_variable_type(
-			Documenter(Bar, ["Bar", 'h'], Analyzer({("Bar", 'h'): "Union[str, float, int]"}))
-			) == ":py:class:`~typing.Union`\\[:py:class:`str`, :py:class:`float`, :py:class:`int`]"
-	assert get_variable_type(
-			Documenter(Bar, ["Bar", 'h'], Analyzer({("Bar", 'h'): "'Union[str, float, int]'"}))
-			) == ":py:class:`~typing.Union`\\[:py:class:`str`, :py:class:`float`, :py:class:`int`]"
-	assert get_variable_type(
-			Documenter(Bar, ["Bar", 'h'], Analyzer({("Bar", 'h'): '"Union[str, float, int]"'}))
-			) == ":py:class:`~typing.Union`\\[:py:class:`str`, :py:class:`float`, :py:class:`int`]"
+		# Failed forward reference throws everything else out of whack
+		((Documenter(Bar, ["Bar", 'd'], Analyzer({}))), ":py:obj:`~.float`"),
+		((Documenter(Bar, ["Bar", 'e'], Analyzer({}))), ":py:obj:`~.List`"),
+		((Documenter(Bar, ["Bar", 'f'], Analyzer({}))), ":py:obj:`~.Set`"),
+		((Documenter(Bar, ["Bar", 'g'], Analyzer({}))), ''),
+		# ((Documenter(Bar, ["Bar", "g"], Analyzer({("Bar", "g"): "Secret"}))), ":py:class:`~domdf_python_tools.secrets.Secret`"),
+		(
+				(Documenter(Bar, ["Bar", 'h'], Analyzer({("Bar", 'h'): "'Secret'"}))),
+				":py:class:`~domdf_python_tools.secrets.Secret`",
+				),
+		(
+				(Documenter(Bar, ["Bar", 'h'], Analyzer({("Bar", 'h'): '"Secret"'}))),
+				":py:class:`~domdf_python_tools.secrets.Secret`",
+				),
+		(
+				(Documenter(Bar, ["Bar", 'h'], Analyzer({("Bar", 'h'): "Union[str, float, int]"}))),
+				":py:class:`~typing.Union`\\[:py:class:`str`, :py:class:`float`, :py:class:`int`]",
+				),
+		(
+				(Documenter(Bar, ["Bar", 'h'], Analyzer({("Bar", 'h'): "'Union[str, float, int]'"}))),
+				":py:class:`~typing.Union`\\[:py:class:`str`, :py:class:`float`, :py:class:`int`]",
+				),
+		(
+				(Documenter(Bar, ["Bar", 'h'], Analyzer({("Bar", 'h'): '"Union[str, float, int]"'}))),
+				":py:class:`~typing.Union`\\[:py:class:`str`, :py:class:`float`, :py:class:`int`]",
+				),
+		(
+				(Documenter("Bar", ["Bar", 'f'], Analyzer({}))),  # type: ignore[arg-type]
+				'',
+				),
+		]
 
-	assert get_variable_type(Documenter("Bar", ["Bar", 'f'], Analyzer({}))) == ''
+
+@pytest.mark.parametrize("documenter, expected", variable_params)
+def test_get_variable_type(documenter: Documenter, expected: str):
+	assert get_variable_type(documenter) == expected  # type: ignore[arg-type]
 
 
 def test_setup():
